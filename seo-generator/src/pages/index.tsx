99937b71dc808e22a94eb14915e72b72c854a945
AllCz29 <allancor29@gmail.com>
2023-06-13 10:53:18 -0600

    Update: clean index

--- seo-generator/src/pages/index.tsx
+++ seo-generator/src/pages/index.tsx
@@ -1,28 +1,10 @@
-import fetchCompletion from "@/api/GPTAPI";
-import { useEffect, useState } from "react";
-
+import SeoApp from "@/root/components/SeoApp/SeoApp";
 
 export default function Home() {
-
-  const [response, setResponse] = useState('');
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const prompt = "fine";
-      const completion = await fetchCompletion(prompt);
-      setResponse(completion);
-    };
-    fetchData();
-  }, []);
   return (
-    <main className="p4">
-      <h1 className="p-10">
-        SEO Keyword Generator
-      </h1>
-      <p>{response || "catgando..."}</p>
+    <main className="flex min-h-screen flex-col items-center justify-between ">
 
-
-      
+        <SeoApp />
     </main>
-  )
+  );
 }
