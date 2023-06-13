eb7023c4f9e8a9888c3e1f34eb7c8aa209626cd4
AllCz29 <allancor29@gmail.com>
2023-06-12 20:10:37 -0600

    create: context provider

---
+++ seo-generator/src/pages/context/context.ts
@@ -0,0 +1,61 @@
+import React, { createContext, useContext, useState } from 'react';
+
+export interface keywordSEO {
+  id: number;
+  keyword: string;
+}
+
+interface keywordContextType {
+  keywords: keywordSEO[];
+  selectedKeywords: keywordSEO;
+  setKeyword: React.Dispatch<React.SetStateAction<keywordSEO[]>>;
+  setSelectedKeywords: (selectedKeywords: keywordSEO) => void;
+  deleteKeyword: (id: number) => void;
+}
+
+export const KeywordContext = createContext<keywordContextType>({
+  keywords: [],
+  selectedKeywords: { keyword: '', id: 0 },
+  setKeyword: () => {},
+  setSelectedKeywords: () => {},
+  deleteKeyword: () => {},
+});
+
+export interface CustomContextProviderProps {
+  children: JSX.Element | JSX.Element[];
+}
+
+const KeywordContextProvider: React.FC<CustomContextProviderProps> = ({ children }) => {
+  const [keywords, setKeywords] = useState<keywordSEO[]>([]);
+  const [selectedKeywords, setSelectedKeywords] = useState<keywordSEO>({ keyword: '', id: 0 });
+
+    
+  const setKeyword = (id: number) => {
+    if(id===0){
+        setSelectedKeywords({ keyword: '', id: 0 });
+    }
+    else{
+        const changeKeyword = keywords.filter((keyword) => keyword.id === id)[0];
+        setSelectedKeywords(changeKeyword);
+  };
+  }
+
+  const deleteKeyword = (id: number) => {
+    setKeywords((prevKeywords) => prevKeywords.filter((keyword) => keyword.id !== id));
+  };
+
+  return (
+    <KeywordContext.Provider
+      value={{ deleteKeyword }}
+    >
+      {children}
+    </KeywordContext.Provider>
+  );
+
+  
+};
+
+
+export const useKeywordContext = () => useContext(KeywordContext);
+
+export default KeywordContextProvider;
