98827ed5212f6b5942ecdc8810fe8b1033c421a0
AllCz29 <allancor29@gmail.com>
2023-06-13 14:34:13 -0600

    update: add VerticalLinearStepper component

---
+++ seo-generator/src/root/components/SeoApp/Stepper/InputPromtp.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Pillsbadge from "../MainTopic/Pillsbadge";
+import VerticalLinearStepper from "./InstructionsStepper";
+
+function InputPromtp() {
+  return (
+    <div className="max-w-xl pt-20">
+      <VerticalLinearStepper />
+    </div>
+  );
+}
+
+export default InputPromtp;
