e7f11beecb4c738a1d41931f276b63ffe7137ed1
AllCz29 <allancor29@gmail.com>
2023-06-13 14:33:51 -0600

    Update: add component stepper

--- seo-generator/src/root/components/SeoApp/SeoApp.tsx
+++ seo-generator/src/root/components/SeoApp/SeoApp.tsx
@@ -1,17 +1,12 @@
 import React, { useEffect } from "react";
-import InputPromtp from "./InputPromtp";
-import { useKeywordContext } from "@/context/Context";
-import GetInitialSuggestions from "./GetInitialSuggestions";
-
+import VerticalLinearStepper from "./Stepper/InstructionsStepper";
 
 function SeoApp() {
-  const { initialKeywords } = useKeywordContext();
   return (
-    <div className="ml-2 mr-2 app-container">
-      <InputPromtp />
-      <GetInitialSuggestions />
+    <div className="ml-2 mr-2 mt-10 p-5 app-container">
+      <VerticalLinearStepper />
+
     </div>
   );
 }
-
 export default SeoApp;
