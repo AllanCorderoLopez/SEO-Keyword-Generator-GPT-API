64d85e0d9e1405820b0882bc11e2b27fb8f2c3f7
AllCz29 <allancor29@gmail.com>
2023-06-13 10:52:45 -0600

    Update: contextProvider in _App

--- seo-generator/src/pages/_app.tsx
+++ seo-generator/src/pages/_app.tsx
@@ -1,6 +1,11 @@
+import KeywordContextProvider, { KeywordContext } from '@/context/Context'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return( 
+  <KeywordContextProvider>
+    <Component {...pageProps} />
+  </KeywordContextProvider>
+  )
 }
