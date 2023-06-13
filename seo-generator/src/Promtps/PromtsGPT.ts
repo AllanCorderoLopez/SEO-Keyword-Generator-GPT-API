16c5056ea4d2de0f4a0c510ecaa4245dddfd8239
AllCz29 <allancor29@gmail.com>
2023-06-13 10:53:46 -0600

    Create: InitialSuggestionsPromtp to use API

---
+++ seo-generator/src/Promtps/PromtsGPT.ts
@@ -0,0 +1,3 @@
+export const getInitialSuggestionsPromtp = () => {
+    return "genere una lista de 7 palabras clave, más cotidianas y comunes en sitios web que funcionen como base para un tema pricipal de una página web para el uso de posicionamiento web, importante seguir este formato: [keyword1 , keyword2 , keyword3 ] los temas deben ser variados y de diferentes usos, no utilices palabras relacionadas a la palabra 'SEO' ni uses palabras extensas"; 
+};
