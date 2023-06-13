6607ebf75b17b135912a39eade14be8068f8d7b9
AllCz29 <allancor29@gmail.com>
2023-06-12 22:45:27 -0600

    update: test api and dependencies

--- seo-generator/src/pages/index.tsx
+++ seo-generator/src/pages/index.tsx
@@ -1,11 +1,28 @@
+import fetchCompletion from "@/api/GPTAPI";
+import { useEffect, useState } from "react";
 
 
 export default function Home() {
+
+  const [response, setResponse] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const prompt = "fine";
+      const completion = await fetchCompletion(prompt);
+      setResponse(completion);
+    };
+    fetchData();
+  }, []);
   return (
     <main className="p4">
       <h1 className="p-10">
         SEO Keyword Generator
       </h1>
+      <p>{response || "catgando..."}</p>
+
+
+      
     </main>
   )
 }
