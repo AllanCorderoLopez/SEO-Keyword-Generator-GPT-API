78cb27acf3e7372dbe025c99d62760fa66dd72f5
AllCz29 <allancor29@gmail.com>
2023-06-14 11:02:14 -0600

    Update: ordering components

--- seo-generator/src/root/components/SeoApp/SeoApp.tsx
+++ seo-generator/src/root/components/SeoApp/SeoApp.tsx
@@ -5,14 +5,35 @@ import { useKeywordContext } from "@/context/Context";
 import { getInitialSuggestionsPromtp } from "@/Promtps/PromtsGPT";
 import PhraseList from "./PhraseList";
 
+import Stepper from "./Stepper/Stepper";
 
 function SeoApp() {
   return (
-    <div className="ml-2 mr-2 mt-10 p-5 app-container">
-      <VerticalLinearStepper />
+    <div>
 
-      <PhraseList/>
+    
+    <div className="mt-10 w-full flex justify-center items-center app-container">
+      <div className="w-full h-full">
+        <img
+          src="https://images.pexels.com/photos/39284/macbook-apple-imac-computer-39284.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt="Macbook"
+          className="img"
+        />
+      </div>
 
+
+
+      <div className="w-full steps-container">
+        <h1 className="flex items-center justify-center h-full px-10">
+          <Stepper />
+        </h1>
+      </div>
+      {/* <VerticalLinearStepper /> */}
+    </div>
+    <div className="pt-20">
+
+    </div>
+    <PhraseList/>
     </div>
   );
 };
