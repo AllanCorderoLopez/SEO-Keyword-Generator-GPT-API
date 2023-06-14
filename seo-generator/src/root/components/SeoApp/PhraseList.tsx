74cd2d20af03724cc0ffdb1c729eccdfebf5b14c
MayronJ <mjiarr.mj@gmail.com>
2023-06-14 13:46:51 -0600

    Create the method for increment all phrases

--- seo-generator/src/root/components/SeoApp/PhraseList.tsx
+++ seo-generator/src/root/components/SeoApp/PhraseList.tsx
@@ -1,5 +1,5 @@
 import fetchCompletion from "@/api/GPTAPI";
-import { getIncrementKeyword, getMiddleKeywords, initialInput, keywordSEO, phraseInterest } from "@/context/Context";
+import {  getIncrementPhrase, getMiddleKeywords, initialInput, keywordSEO, phraseInterest } from "@/context/Context";
 import React, { useEffect, useState } from "react";
 
 function PhraseList() {
@@ -42,22 +42,22 @@ function PhraseList() {
     const handleGetMoreSuggestions = async () => {
         const prompt = getMiddleKeywords(moreInputs);
         const completion = await fetchCompletion(prompt);
-      
+
         const phraseArray = completion.split("#");
-      
+
         const phraseSEOArray: phraseInterest[] = phraseArray
-          .filter((p: string) => p.trim() !== "") // Filtrar cadenas vacías
-          .map((p: string, index: number) => ({
-            id: phrasesThatInterest.length + index + 1, // Generar un nuevo ID único
-            phrase: p.trim(),
-          }));
-      
+            .filter((p: string) => p.trim() !== "") // Filtrar cadenas vacías
+            .map((p: string, index: number) => ({
+                id: phrasesThatInterest.length + index + 1, // Generar un nuevo ID único
+                phrase: p.trim(),
+            }));
+
         setPhrasesThatIntrest((prevPhrases) => [...prevPhrases, ...phraseSEOArray]); // Concatenar correctamente las frases generadas
         console.log("-----getMore--------");
         console.log(phrasesThatInterest);
-      };
+    };
     const handleIncrementPhrase = async (selectedPhrase: phraseInterest) => {
-        const prompt = getIncrementKeyword(selectedPhrase);
+        const prompt = getIncrementPhrase(selectedPhrase);
         const newPhrase = await fetchCompletion(prompt);
 
         const updatedPhrases = phrasesThatInterest.map((phrase) => {
@@ -71,11 +71,35 @@ function PhraseList() {
         });
         setPhrasesThatIntrest(updatedPhrases);
     }
+    const handleIncrementAllPhrases = async (selectedPhrases: phraseInterest[]) => {
+  const updatedPhrases: phraseInterest[] = [];
+  
+  await Promise.all(
+    phrasesThatInterest.map(async (phrase) => {
+      if (selectedPhrases.some((selected) => selected.id === phrase.id)) {
+        const prompt = getIncrementPhrase(phrase);
+        const newPhrase = await fetchCompletion(prompt);
+        const updatedPhrase = {
+          ...phrase,
+          phrase: newPhrase ? newPhrase.trim() : '',
+        };
+        updatedPhrases.push(updatedPhrase);
+      } else {
+        updatedPhrases.push(phrase);
+      }
+    })
+  );
+
+  setPhrasesThatIntrest(updatedPhrases);
+};
+
+      
+
     useEffect(() => {
-        
-      }, [phrasesThatInterest]);
+    }, [phrasesThatInterest]);
+
     const handleDeletePhrase = (selectedPhrase: phraseInterest) => {
-        
+
         const updatedPhrases = phrasesThatInterest.filter(
             (phrase) => phrase.phrase !== selectedPhrase.phrase
         );
@@ -92,18 +116,25 @@ function PhraseList() {
         <div className="rounded-lg border-2 border-red-500 p-4">
             <h3>Lista de frases</h3>
             <div >
-                <button onClick={handleGetInitialSuggestions}>
+                <button onClick={handleGetInitialSuggestions} className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
                     Generar Frases
                 </button>
+                <span>  </span>
+                <button onClick={() => handleIncrementAllPhrases(phrasesThatInterest)} className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"> Incrementar todas las frases</button>
+
                 {phrasesThatInterest.slice(0, amountOfKeyword).map((phrase, index) => (
                     <div key={index} className="my-2 rounded-lg border-2 border-blue-500 p-4">
                         <div>{phrase.phrase}</div>
                         <div className="">
-                            <button onClick={() => handleIncrementPhrase(phrase)}>Incrementar</button>
+                            <button onClick={() => handleIncrementPhrase(phrase)}>Modificar </button>
+                            <span> </span>
                             <button onClick={() => handleDeletePhrase(phrase)}>Eliminar</button>
                         </div>
                     </div>
                 ))}
+                <button onClick={handleGetInitialSuggestions} className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
+                    Siguiente
+                </button>
             </div>
         </div>
     )
