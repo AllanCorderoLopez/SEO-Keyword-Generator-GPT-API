4b2fe0ba4322658a642a243eb3e83095143d27e8
AllCz29 <allancor29@gmail.com>
2023-06-14 11:02:30 -0600

    Create: suggestions in pills

---
+++ seo-generator/src/root/components/SeoApp/MainTopic/Suggestions.tsx
@@ -0,0 +1,30 @@
+import { useKeywordContext } from "@/context/Context";
+import React from "react";
+import { FaSyncAlt } from "react-icons/fa";
+
+function Suggestions() {
+  const { initialKeywords, setMainTopic, mainTopic } = useKeywordContext();
+  const handleSetTopic = (keyword: string) => () => {
+    setMainTopic(keyword);
+  };
+  
+  return (
+    <div className="flex pt-5">
+      {initialKeywords.map((keyword) => (
+        <div className="px-2 slide-top" key={keyword.id}>
+          <label className="flex  text-sm font-medium text-gray-900 dark:text-white">
+            <button
+              onClick={handleSetTopic(keyword.keyword)}
+              id="badge-dismiss-dark"
+              className="btn-suggestions-topic focus:outline-none text-white font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900 animate-slide-top"
+            >
+              {keyword.keyword}
+            </button>
+          </label>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Suggestions;
