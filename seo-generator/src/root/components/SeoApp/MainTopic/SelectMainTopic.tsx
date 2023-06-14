ee521791d3fcc8d9d4b862bbf82f0abfc5fd388f
AllCz29 <allancor29@gmail.com>
2023-06-14 11:02:51 -0600

    create: form select main topic

---
+++ seo-generator/src/root/components/SeoApp/MainTopic/SelectMainTopic.tsx
@@ -0,0 +1,84 @@
+import { getInitialSuggestionsPromtp } from "@/Promtps/PromtsGPT";
+import fetchCompletion from "@/api/GPTAPI";
+import { keywordSEO } from "@/context/Context";
+import { useKeywordContext } from "../../../../context/Context";
+import React, { useEffect, useState } from "react";
+import { FaRegLightbulb, FaSyncAlt } from "react-icons/fa";
+import Suggestions from "./Suggestions";
+
+function SelectMainTopic() {
+  const { setInitialKeywords, setIsTopicsGenerated, isTopicsGenerated } =
+    useKeywordContext();
+  const { initialKeywords, setMainTopic, mainTopic } = useKeywordContext();
+  const handleFieldChange = (fieldName: string, value: string) => {
+    setMainTopic(value);
+  };
+
+  const [initialSuggestions, setInitialSuggestions] = useState("");
+
+  const GetInitialSuggestions = async () => {
+    const prompt = getInitialSuggestionsPromtp();
+    const completion = await fetchCompletion(prompt);
+    console.log(completion);
+    setInitialSuggestions(completion);
+    const cleanedKeywords = completion.replace(/^\[|\[|\]|\].|\.|\,$/g, " ");
+    const keywordArray = cleanedKeywords.split(", ");
+    const keywordSEOArray: keywordSEO[] = keywordArray.map(
+      (keyword: string, index: number) => ({
+        id: index + 1,
+        keyword: keyword.trim(),
+      })
+    );
+    setInitialKeywords(keywordSEOArray);
+    setIsTopicsGenerated(true);
+    return 0;
+  };
+
+  return (
+    <div className="step-container">
+      <h1 className="font-bold text-4xl">
+        ¿Qué tipo de negocio estás desarrollando?
+      </h1>
+      <div className="pt-10">
+        <input
+          value={mainTopic}
+          onChange={(e) => handleFieldChange("lastname", e.target.value)}
+          type="email"
+          id="helper-text"
+          aria-describedby="helper-text-explanation"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+          placeholder="Escribe el tema principal de su sitio"
+        />
+      </div>
+      <div className="block">
+        <div className="flex">
+          <h1 className="font-medium text-medium text-gray-800 pt-5">
+            ¿No está seguro?
+          </h1>
+          <div className="flex h1-color-suggestion">
+            <button
+              onClick={() => GetInitialSuggestions()}
+              className="font-medium text-medium px-2 pt-4 "
+            >
+              Mira algunas sugerencias
+            </button>
+            <div className="pt-6 flex">
+              <FaRegLightbulb />
+            </div>
+          </div>
+
+          <div className="font-medium text-medium pt-5 pl-5 h1-color-suggestion">
+            <button>
+              <FaSyncAlt />
+            </button>
+          </div>
+        </div>
+        <div>
+          <Suggestions />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SelectMainTopic;
