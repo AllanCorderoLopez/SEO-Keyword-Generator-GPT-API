a689df2cbcbd62f9c27106ef6ea373960d3169e9
MayronJ <mjiarr.mj@gmail.com>
2023-06-14 10:37:43 -0600

    update: add the component PhraseList to SeoApp

--- seo-generator/src/root/components/SeoApp/SeoApp.tsx
+++ seo-generator/src/root/components/SeoApp/SeoApp.tsx
@@ -1,12 +1,19 @@
 import React, { useEffect } from "react";
 import VerticalLinearStepper from "./Stepper/InstructionsStepper";
+import InputPromtp from "./Stepper/InputPromtp";
+import { useKeywordContext } from "@/context/Context";
+import { getInitialSuggestionsPromtp } from "@/Promtps/PromtsGPT";
+import PhraseList from "./PhraseList";
+
 
 function SeoApp() {
   return (
     <div className="ml-2 mr-2 mt-10 p-5 app-container">
       <VerticalLinearStepper />
 
+      <PhraseList/>
+
     </div>
   );
-}
+};
 export default SeoApp;
