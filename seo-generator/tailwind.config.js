a58cb0e1c817ebf1c916086b547733356de7d4eb
AllCz29 <allancor29@gmail.com>
2023-06-14 10:51:42 -0600

    Update: imports tailwind

--- seo-generator/tailwind.config.js
+++ seo-generator/tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT({
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/root/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -9,12 +11,24 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+
+      },
+      colors: {
+        sky: {
+          50: "#f0f9ff",
+          100: "#e0f2fe",
+          200: "#bae6fd",
+          300: "#7dd3fc",
+          400: "#38bdf8",
+          500: "#0ea5e9",
+          600: "#0284c7",
+          700: "#0369a1",
+          800: "#075985",
+          900: "#0c4a6e",
+        },
       },
     },
   },
   plugins: [],
-}
+});
 
