76fe4b679ac7f9f7c27dda11f0e77a23b764b8d1
AllCz29 <allancor29@gmail.com>
2023-06-16 08:17:42 -0600

    update: add toast

--- seo-generator/src/pages/index.tsx
+++ seo-generator/src/pages/index.tsx
@@ -1,13 +1,27 @@
 import Footer from "@/root/components/SeoApp/Footer/Footer";
 import SeoApp from "@/root/components/SeoApp/SeoApp";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between ">
-
+    <div>
+<ToastContainer
+position="bottom-left"
+autoClose={2000}
+hideProgressBar={false}
+newestOnTop={false}
+closeOnClick
+rtl={false}
+pauseOnFocusLoss
+draggable
+pauseOnHover
+theme="light"
+/>
+      <main className="flex min-h-screen flex-col items-center justify-between ">
         <SeoApp />
-        <Footer/>
-    </main>
-    
+        <Footer />
+      </main>
+    </div>
   );
 }
