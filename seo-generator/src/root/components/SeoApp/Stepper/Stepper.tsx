125bbfc22b980a15254808dcaba2910f6971b033
AllCz29 <allancor29@gmail.com>
2023-06-16 08:10:37 -0600

    Update: stepper component

--- seo-generator/src/root/components/SeoApp/Stepper/Stepper.tsx
+++ seo-generator/src/root/components/SeoApp/Stepper/Stepper.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import { Stepper, Step, Button } from "@material-tailwind/react";
 import SelectMainTopic from "../MainTopic/SelectMainTopic";
 import SelectBadWords from "../BadWords/SelectBadWords";
+import SelectIntentions from "../Intentions/SelectIntentions";
+import { useKeywordContext } from "@/context/Context";
+import PhraseList from "../Phrases/PhraseList";
+import MiddlephraseList from "../Phrases/MiddlephraseList";
 
 const Step1Component = () => (
   <div>
@@ -10,25 +14,28 @@ const Step1Component = () => (
     </h1>
   </div>
 );
-const Step2Component = () => <div>Contenido del Paso 2</div>;
-const Step3Component = () => <div>Contenido del Paso 3</div>;
 
-const steps = [<SelectMainTopic />, <SelectBadWords />, <Step3Component />];
+const steps = [
+  <SelectMainTopic />,
+  <SelectBadWords />,
+  <SelectIntentions />,
+  <MiddlephraseList />,
+];
 
 export default function Example() {
-  const [activeStep, setActiveStep] = React.useState(0);
+  const { setActiveStep, activeStep } = useKeywordContext();
 
   const handleNext = () => setActiveStep((cur) => cur + 1);
   const handlePrev = () => setActiveStep((cur) => cur - 1);
 
   return (
-    <div className=" py-4 px-8">
-      <Stepper activeStep={activeStep}>
+    <div className=" py-4 px-8 ">
+      <Stepper activeStep={activeStep} className="hidden">
         <Step
-          className={`stepper ${activeStep === 0 ? "active" : "inactive"}`}
+          className={`stepper ${activeStep === 0 ? "active" : "sd"}step-pill`}
           onClick={() => setActiveStep(0)}
         >
-          1
+          
         </Step>
         <Step
           className={`stepper ${activeStep === 1 ? "active" : "inactive"}`}
@@ -42,25 +49,17 @@ export default function Example() {
         >
           3
         </Step>
+        <Step
+          className={`stepper ${activeStep === 3 ? "active" : "inactive"}`}
+          onClick={() => setActiveStep(3)}
+        >
+          4
+        </Step>
+        
       </Stepper>
 
       <div className="mt-16">{steps[activeStep]}</div>
-      <div className="mt-16 flex justify-between">
-        <Button
-          className="btn-prev-step"
-          onClick={handlePrev}
-          disabled={activeStep === 0}
-        >
-          Atrás
-        </Button>
-        <Button
-          className="btn-next-step"
-          onClick={handleNext}
-          disabled={activeStep === steps.length - 1}
-        >
-          Siguiente
-        </Button>
-      </div>
+      <div className="mt-16 flex justify-between"></div>
     </div>
   );
 }
