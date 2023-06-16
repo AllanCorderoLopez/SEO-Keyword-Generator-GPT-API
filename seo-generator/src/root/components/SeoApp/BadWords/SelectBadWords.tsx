81ac19218ce61518a46601295ee7d660122ceed4
AllCz29 <allancor29@gmail.com>
2023-06-16 08:17:18 -0600

    Update: add states context

--- seo-generator/src/root/components/SeoApp/BadWords/SelectBadWords.tsx
+++ seo-generator/src/root/components/SeoApp/BadWords/SelectBadWords.tsx
@@ -1,16 +1,26 @@
 import { useEffect, useState } from "react";
 import { keywordSEO, useKeywordContext } from "@/context/Context";
 import React from "react";
-import Masonry from 'react-masonry-css';
+import Masonry from "react-masonry-css";
 import { FaCheck, FaPlus } from "react-icons/fa";
 import { AiOutlinePlus } from "react-icons/ai";
 import { BsCheckLg } from "react-icons/bs";
 import { getInitialBadWordsPrompt } from "@/Promtps/PromtsGPT";
 import fetchCompletion from "@/api/GPTAPI";
+import Pillsbadge from "../MainTopic/Pillsbadge";
+import PillsLoader from "../PillsLoader";
 function SelectBadWords() {
-  const {  setMainTopic, mainTopic, setInitialBadKeywords, badKeywords } = useKeywordContext();
+  const {
+    isBadKeyWordsGenerated,
+    mainTopic,
+    setActiveStep,
+    setInitialBadKeywords,
+    badKeywords,
+    setSelectedBadKeywords,
+    selectedBadKeywords,
+    setIsBadKeyWordsGenerated,
+  } = useKeywordContext();
   const [initialBadSuggestions, setInitialBadSuggestions] = useState("");
-
 
   const GetInitialSuggestions = async () => {
     const prompt = getInitialBadWordsPrompt(mainTopic);
@@ -25,52 +35,82 @@ function SelectBadWords() {
       })
     );
     setInitialBadKeywords(keywordSEOArray);
-    console.log(badKeywords);
-    console.log(completion);
+    setIsBadKeyWordsGenerated(true);
     return 0;
-  }
-
+  };
+  const handleNext = () => setActiveStep((cur) => cur + 1);
+  const handlePrev = () => setActiveStep((cur) => cur - 1);
   useEffect(() => {
-    if (mainTopic !== "") {
-        GetInitialSuggestions();
+    if (isBadKeyWordsGenerated === false) {
+      GetInitialSuggestions();
     }
-    }, [mainTopic]);
+  }, [isBadKeyWordsGenerated]);
 
-  const [selectedKeywords, setSelectedKeywords] = useState<string[]>([]);
 
-  const handleKeywordClick = (keyword: string) => {
-    if (selectedKeywords.includes(keyword)) {
-      setSelectedKeywords(selectedKeywords.filter((kw) => kw !== keyword));
+  const handleKeywordClick = (keyword: keywordSEO) => {
+    if (selectedBadKeywords.includes(keyword)) {
+      setSelectedBadKeywords(selectedBadKeywords.filter((kw) => kw !== keyword));
     } else {
-      setSelectedKeywords([...selectedKeywords, keyword]);
+      setSelectedBadKeywords([...selectedBadKeywords, keyword]);
     }
   };
 
   return (
     <div className="step-container ">
-      <h1 className="font-bold text-2xl">
+      <h1 className="font-bold text-4xl">
         Palabras que no quieres que aparezcan en tu contenido.
+        
       </h1>
 
       <h1 className="font-medium text-medium text-gray-800 pt-2">
         Son importantes para la reputación de tu sitio.
       </h1>
-      <div className="pt-14">
-        {badKeywords.map((keyword) => (
-          <button
-            id={keyword.id.toString()}
-            className={` mx-2 focus:outline-none text-white font-medium rounded-lg text-sm px-5 py-2 mb-2 ${selectedKeywords.includes(keyword.keyword) ? "btn-suggestions-bad-clicked" : "btn-suggestions-bad"}`}
-            key={keyword.id}
-            onClick={() => handleKeywordClick(keyword.keyword)}
-            
-          >
-            <div className="flex">
-            {keyword.keyword}
-            {selectedKeywords.includes(keyword.keyword) ?
-            <BsCheckLg className="ml-3 mt-1" /> : <AiOutlinePlus className="ml-3 mt-1" />}
-            </div>
-          </button>
-        ))}
+      <div className="pt-14 animate-slide-top">
+        {isBadKeyWordsGenerated === false ? (
+          <PillsLoader />
+        ) : (
+          badKeywords.map((keyword) => (
+            <button
+              id={keyword.id.toString()}
+              className={` mx-2 focus:outline-none text-white font-medium rounded-lg text-sm px-5 py-2 mb-2 ${
+                selectedBadKeywords.includes(keyword)
+                  ? "btn-suggestions-bad-clicked"
+                  : "btn-suggestions-bad"
+              }`}
+              key={keyword.id}
+              onClick={() => handleKeywordClick(keyword)}
+            >
+              <div className="flex">
+                {keyword.keyword}
+                {selectedBadKeywords.includes(keyword) ? (
+                  <BsCheckLg className="ml-3 mt-1" />
+                ) : (
+                  <AiOutlinePlus className="ml-3 mt-1" />
+                )}
+              </div>
+            </button>
+          ))
+        )}
+      </div>
+      <div className="flex mt-10 justify-between	mr-10">
+        <button
+          onClick={handlePrev}
+          className={`mt-1 ml-2 btn-suggestions-topic focus:outline-none text-white font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900 animate-slide-top ${
+            mainTopic !== "" ? "" : "opacity-60"
+          }`}
+          disabled={badKeywords.length === 0}
+        >
+          Atrás
+        </button>
+        <button
+          onClick={handleNext}
+          className={`mt-1 ml-2 btn-suggestions-topic focus:outline-none text-white font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900 animate-slide-top ${
+            selectedBadKeywords.length !== 0 ? "" : "opacity-60"
+          }`}
+          disabled={selectedBadKeywords.length === 0 }
+        >
+          Siguiente
+        </button>
       </div>
     </div>
   );
