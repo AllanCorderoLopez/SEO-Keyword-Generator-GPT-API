45314a2061478d7e85c271c0e890868eb9e7a583
AllCz29 <allancor29@gmail.com>
2023-06-16 08:12:46 -0600

    updte: add states in context

--- seo-generator/src/root/components/SeoApp/Phrases/PhraseList.tsx
+++ seo-generator/src/root/components/SeoApp/Phrases/PhraseList.tsx
@@ -1,133 +1,256 @@
 import fetchCompletion from "@/api/GPTAPI";
-import { ArrayOfPhrases, getIncrementPhrase, getMiddleKeywords, initialInput, keywordSEO, phraseInterest } from "@/context/Context";
+import {
+  ArrayOfPhrases,
+  getIncrementPhrase,
+  getMiddleKeywords,
+  initialInput,
+  keywordSEO,
+  phareList,
+  phraseInterest,
+  useKeywordContext,
+} from "@/context/Context";
 import React, { useEffect, useState } from "react";
 import FinalList from "./FinalList";
+import { FaPlus, FaTimes, FaTrash } from "react-icons/fa";
 
 function PhraseList() {
-    const phraseList: string[] = ["Viajes", "cancelar, enfermedad, atraso, muerte, perdida, robo", "conocer nuevos lugares", "3"];
-    const initialKeywordsArray: keywordSEO[] = [];
-    const badWordsArray: string[] = phraseList[1].split(", ");
-    const intentionsArray: string[] = [phraseList[2]];
-    const amountOfKeyword: number = parseInt(phraseList[3]);
-    const [phrasesThatInterest, setPhrasesThatIntrest] = useState<phraseInterest[]>([]);
-    const [finalListPhrases, setFinalListPhrases] = useState<phraseInterest[]>([]);
-    const [isActive, setIsActive] = useState(false);
-    const initialInputs: initialInput = {
-        mainTopic: phraseList[0],
-        badWords: badWordsArray,
-        intentions: intentionsArray,
-        amountOfKeywords: amountOfKeyword + 2,
-        initialKeywords: initialKeywordsArray,
+  const {
+    setPhrases,
+    phrases,
+    mainTopic,
+    selectedBadKeywords,
+    selectedIntentions,
+    setPhrasesThatIntrest,
+    phrasesThatIntrest,
+    setIsActive,
+    isActive,
+
+    setIsLoadedPhrases,
+    isLoadedPhrases,
+  } = useKeywordContext();
+
+  const initialKeywordsArray: keywordSEO[] = [];
+  const badWordsArray: keywordSEO[] = phrases.badWords;
+  const intentionsArray: keywordSEO[] = phrases.intentions;
+  const amountOfKeyword: number = phrases.amountOfKeywords;
+
+  const [finalListPhrases, setFinalListPhrases] = useState<phraseInterest[]>(
+    []
+  );
+    const [isBlocked, setIsBlocked] = useState<boolean>(false);
+  const initialInputs: initialInput = {
+    mainTopic: phrases.topic,
+    badWords: badWordsArray,
+    intentions: intentionsArray,
+    amountOfKeywords: amountOfKeyword + 2,
+    initialKeywords: initialKeywordsArray,
+  };
+
+  const moreInputs: initialInput = {
+    mainTopic: phrases.topic,
+    badWords: badWordsArray,
+    intentions: intentionsArray,
+    amountOfKeywords: 2,
+    initialKeywords: initialKeywordsArray,
+  };
+
+  const handleGetInitialSuggestions = async () => {
+    //DELETE THIS
+    const phraseList: phareList = {
+      topic: mainTopic,
+      badWords: selectedBadKeywords,
+      intentions: selectedIntentions,
+      amountOfKeywords: 15,
     };
-    const moreInputs: initialInput = {
-        mainTopic: phraseList[0],
-        badWords: badWordsArray,
-        intentions: intentionsArray,
-        amountOfKeywords: 2,
-        initialKeywords: initialKeywordsArray,
-    };
-    const handleGetInitialSuggestions = async () => {
-        const prompt = getMiddleKeywords(initialInputs);
-        const completion = await fetchCompletion(prompt);
-        const phraseArray = completion.split("#");
-        const phraseSEOArray: phraseInterest[] = phraseArray
-            .filter((p: string) => p.trim() !== "")
-            .map((p: string, index: number) => ({
-                id: index + 1,
-                phrase: p.trim(),
-            }));
-        setPhrasesThatIntrest(phraseSEOArray);
-        setIsActive(false);
-    }
-    const handleGetMoreSuggestions = async () => {
-        const prompt = getMiddleKeywords(moreInputs);
-        const completion = await fetchCompletion(prompt);
-        const phraseArray = completion.split("#");
-        const phraseSEOArray: phraseInterest[] = phraseArray
-            .filter((p: string) => p.trim() !== "")
-            .map((p: string, index: number) => ({
-                id: phrasesThatInterest.length + index + 1,
-                phrase: p.trim(),
-            }));
-        setPhrasesThatIntrest((prevPhrases) => [...prevPhrases, ...phraseSEOArray]);
-    };
-    const handleIncrementPhrase = async (selectedPhrase: phraseInterest) => {
-        const prompt = getIncrementPhrase(selectedPhrase);
-        const newPhrase = await fetchCompletion(prompt);
-        const updatedPhrases = phrasesThatInterest.map((phrase) => {
-            if (phrase.id === selectedPhrase.id) {
-                return {
-                    ...phrase,
-                    phrase: newPhrase.trim(),
-                };
-            }
-            return phrase;
-        });
-        setPhrasesThatIntrest(updatedPhrases);
-    };
-    const handleIncrementAllPhrases = async (selectedPhrases: phraseInterest[]) => {
-        const updatedPhrases: phraseInterest[] = [];
-        await Promise.all(
-            phrasesThatInterest.map(async (phrase) => {
-                if (selectedPhrases.some((selected) => selected.id === phrase.id)) {
-                    const prompt = getIncrementPhrase(phrase);
-                    const newPhrase = await fetchCompletion(prompt);
-                    const updatedPhrase = {
-                        ...phrase,
-                        phrase: newPhrase ? newPhrase.trim() : '',
-                    };
-                    updatedPhrases.push(updatedPhrase);
-                } else {
-                    updatedPhrases.push(phrase);
-                }
-            })
-        );
-        setPhrasesThatIntrest(updatedPhrases);
-    };
-    useEffect(() => {
-    }, [phrasesThatInterest]);
-    const handleDeletePhrase = (selectedPhrase: phraseInterest) => {
-        const updatedPhrases = phrasesThatInterest.filter(
-            (phrase) => phrase.phrase !== selectedPhrase.phrase
-        );
-        setPhrasesThatIntrest(updatedPhrases);
-        if ((phrasesThatInterest.length - 1) === amountOfKeyword) {
-            handleGetMoreSuggestions();
+    setPhrases(phraseList);
+    //DELETE THIS ^^^^^
+
+    const prompt = getMiddleKeywords(initialInputs);
+    const completion = await fetchCompletion(prompt);
+    const phraseArray = completion.split("#");
+    const phraseSEOArray: phraseInterest[] = phraseArray
+      .filter((p: string) => p.trim() !== "")
+      .map((p: string, index: number) => ({
+        id: index + 1,
+        phrase: p.trim(),
+      }));
+    setPhrasesThatIntrest(phraseSEOArray);
+    setIsActive(false);
+    console.log("phraseSEOArray", phraseSEOArray);
+    setIsBlocked(false)
+  };
+
+  const handleGetMoreSuggestions = async () => {
+    const prompt = getMiddleKeywords(moreInputs);
+    const completion = await fetchCompletion(prompt);
+    const phraseArray = completion.split("#");
+    const phraseSEOArray: phraseInterest[] = phraseArray
+      .filter((p: string) => p.trim() !== "")
+      .map((p: string, index: number) => ({
+        id: phrasesThatIntrest.length + index + 1,
+        phrase: p.trim(),
+      }));
+    setPhrasesThatIntrest((prevPhrases) => [...prevPhrases, ...phraseSEOArray]);
+  };
+
+  const handleIncrementPhrase = async (selectedPhrase: phraseInterest) => {
+    const prompt = getIncrementPhrase(selectedPhrase);
+    const newPhrase = await fetchCompletion(prompt);
+    const updatedPhrases = phrasesThatIntrest.map((phrase) => {
+      if (phrase.id === selectedPhrase.id) {
+        return {
+          ...phrase,
+          phrase: newPhrase.trim(),
+        };
+      }
+      return phrase;
+    });
+    setPhrasesThatIntrest(updatedPhrases);
+  };
+  const handleIncrementAllPhrases = async (
+    selectedPhrases: phraseInterest[]
+  ) => {
+    const updatedPhrases: phraseInterest[] = [];
+    await Promise.all(
+      phrasesThatIntrest.map(async (phrase) => {
+        if (selectedPhrases.some((selected) => selected.id === phrase.id)) {
+          const prompt = getIncrementPhrase(phrase);
+          const newPhrase = await fetchCompletion(prompt);
+          const updatedPhrase = {
+            ...phrase,
+            phrase: newPhrase ? newPhrase.trim() : "",
+          };
+          updatedPhrases.push(updatedPhrase);
+        } else {
+          updatedPhrases.push(phrase);
         }
-    };
-    const handleClearPhrases = () => {
-        setPhrasesThatIntrest([]);
-    };
-    const handleButtonClick = () => {
-        setFinalListPhrases(phrasesThatInterest.slice(0, amountOfKeyword));
-        setIsActive(true);
-        handleClearPhrases();
-    };
-    return (
-        <div className="rounded-lg border-2 border-red-500 p-4  items-center justify-center ">
-            <h3>Lista de frases</h3>
-            <div className="items-center justify-center">
-                <button onClick={handleGetInitialSuggestions} className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
-                    Generar Frases </button>
-                <span>  </span>
-                <button onClick={() => handleIncrementAllPhrases(phrasesThatInterest)} className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"> Incrementar todas las frases</button>
-                {phrasesThatInterest.slice(0, amountOfKeyword).map((phrase, index) => (
-                    <div key={index} className="my-2 rounded-lg border-2 border-blue-500 p-4">
-                        <div>{phrase.phrase}</div>
-                        <div className="">
-                            <button onClick={() => handleIncrementPhrase(phrase)}>Modificar </button>
-                            <span> </span>
-                            <button onClick={() => handleDeletePhrase(phrase)}>Eliminar</button>
-                        </div>
-                    </div>
-                ))}
-                <div>
-                    <button onClick={handleButtonClick} className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
-                        Listo </button>
-                    {isActive && <FinalList arrayOfPhrases={finalListPhrases} />}
-                </div>
-            </div>
+      })
+    );
+    setPhrasesThatIntrest(updatedPhrases);
+  };
+  useEffect(() => {
+    if (isLoadedPhrases === false) {
+      console.log("isLoadedPhrases", isLoadedPhrases);
+      handleGetInitialSuggestions();
+      setIsLoadedPhrases(true);
+    }
+    console.log("isLoadedPhrases", isLoadedPhrases);
+  }, [phrasesThatIntrest]);
+  const [deletedPhrases, setDeletedPhrases] = useState<phraseInterest[]>([]);
+  const handleDeletePhrase = (
+    selectedPhrase: phraseInterest,
+    index: number
+  ) => {
+    const updatedPhrases = phrasesThatIntrest.filter(
+      (phrase) => phrase.phrase !== selectedPhrase.phrase
+    );
+    const divElement = document.getElementById(index.toString());
+    if (divElement) {
+      divElement.classList.add("fade-out");
+    }
+    setTimeout(() => {
+      setPhrasesThatIntrest(updatedPhrases);
+      if (phrasesThatIntrest.length - 1 === amountOfKeyword) {
+        handleGetMoreSuggestions();
+      }
+      if (divElement) {
+        divElement.classList.remove("fade-out");
+        divElement.classList.add("opacity-1");
+      }
+    }, 700);
+
+    if (phrasesThatIntrest.length === 1) {
+      handleGetInitialSuggestions();
+    }
+  };
+
+  const handleClearPhrases = () => {
+    setPhrasesThatIntrest([]);
+  };
+
+  const handleButtonClick = () => {
+    setFinalListPhrases(phrasesThatIntrest.slice(0, amountOfKeyword));
+    setIsActive(true);
+    handleClearPhrases();
+    setIsBlocked(true);
+  };
+
+  return (
+    <div className="rounded-lg p-4   items-center justify-center ">
+      <button
+        onClick={handleGetInitialSuggestions}
+        className="btn-suggestions-topic focus:outline-none text-white font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900 animate-slide-top"
+      >
+        Generar Frases{" "}
+      </button>
+      <span> </span>
+      <button
+        onClick={() => handleIncrementAllPhrases(phrasesThatIntrest)}
+        className="btn-suggestions-topic focus:outline-none text-white font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900 animate-slide-top"
+      >
+        {" "}
+        Incrementar todas las frases
+      </button>
+      <button
+        onClick={handleButtonClick}
+        className="ml-1 btn-suggestions-topic focus:outline-none text-white font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900 animate-slide-top"
+      >
+        Listo{" "}
+      </button>
+      <div
+        className={`main-modal color-bg2 fixed w-full h-100 inset-0 z-50 overflow-hidden flex justify-center items-center animated fadeIn faster ${
+          phrasesThatIntrest.length === 0 && isBlocked === false?  "" : "hidden"
+        }`}
+      ></div>
+      <div
+        className={`main-modal   fixed w-full h-100 inset-0 z-50 overflow-hidden flex justify-center items-center animated fadeIn faster ${
+          phrasesThatIntrest.length === 0 && isBlocked === false ? "" : "hidden"
+        }`}
+      >
+        <div id="preloader">
+          <div id="loader"></div>
         </div>
-    )
+        <h1 className="pulse-ani font-bold mt-80 text-gray-800 text-2xl">
+          Trabajando en potenciar tu sitio...
+        </h1>
+      </div>
+      <div className="items-center overflow-x-hidden overflow-auto phrases-container justify-center">
+        <div>{isActive && <FinalList arrayOfPhrases={finalListPhrases} />}</div>
+        {phrasesThatIntrest.slice(0, amountOfKeyword).map((phrase, index) => (
+          <div>
+            <div
+              id={index.toString()}
+              key={index}
+              className="phrase-container  mt-5 justify-between flex animate-slide-bottom my-2 rounded-lg p-4 px-6"
+            >
+              <div>
+                <h1 className="font-semibold text-lg h1-phrase">
+                  {phrase.phrase}
+                </h1>
+              </div>
+              <div className="flex  ml-10">
+                <button onClick={() => handleIncrementPhrase(phrase)}>
+                  <div className="plus flex">
+                    <div className="pt-1 pr-2">
+                      <FaPlus />
+                    </div>
+                    <h1 className="font-medium text-base  mr-1">Ampliar</h1>
+                  </div>
+                </button>
+                <button
+                  onClick={() => handleDeletePhrase(phrase, index)}
+                  className="pl-2"
+                >
+                  <div className="delete ">
+                    <FaTimes />
+                  </div>
+                </button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 export default PhraseList;
