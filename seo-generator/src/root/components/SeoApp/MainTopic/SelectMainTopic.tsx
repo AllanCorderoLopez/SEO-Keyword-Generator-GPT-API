7241674e820f0301a54fd45d14024a5af1338124
AllCz29 <allancor29@gmail.com>
2023-06-16 08:15:42 -0600

    update: clear states context

--- seo-generator/src/root/components/SeoApp/MainTopic/SelectMainTopic.tsx
+++ seo-generator/src/root/components/SeoApp/MainTopic/SelectMainTopic.tsx
@@ -2,20 +2,49 @@ import { getInitialSuggestionsPromtp } from "@/Promtps/PromtsGPT";
 import fetchCompletion from "@/api/GPTAPI";
 import { keywordSEO } from "@/context/Context";
 import { useKeywordContext } from "../../../../context/Context";
-import React, { useEffect, useState } from "react";
+import React, { use, useEffect, useState } from "react";
 import { FaRegLightbulb, FaSyncAlt } from "react-icons/fa";
 import Suggestions from "./Suggestions";
+import PillsLoader from "../PillsLoader";
 
 function SelectMainTopic() {
-  const { setInitialKeywords, setIsTopicsGenerated, isTopicsGenerated } =
-    useKeywordContext();
-  const { initialKeywords, setMainTopic, mainTopic } = useKeywordContext();
+
+  const {
+    setInitialKeywords,
+    setIsTopicsGenerated,
+    isTopicsGenerated,
+    setIsBadKeyWordsGenerated,
+    setIsIntentionsGenerated,
+    setActiveStep,
+    setSelectedIntentions,
+    setSelectedBadKeywords,
+  } = useKeywordContext();
+
+  const {
+    setInitialBadKeywords,
+    setInitialIntentions,
+    setMainTopic,
+    mainTopic,
+  } = useKeywordContext();
+
   const handleFieldChange = (fieldName: string, value: string) => {
     setMainTopic(value);
+    setIsBadKeyWordsGenerated(true);
+    setIsIntentionsGenerated(true);
+    setInitialBadKeywords([]);
+    setInitialIntentions([]);
+    setSelectedIntentions([]);
+    setSelectedBadKeywords([]);
+    if (value === "") {
+      setIsBadKeyWordsGenerated(true);
+      setIsIntentionsGenerated(true);
+      setInitialBadKeywords([]);
+      setInitialIntentions([]);
+    }
   };
 
   const [initialSuggestions, setInitialSuggestions] = useState("");
-
+  
   const GetInitialSuggestions = async () => {
     const prompt = getInitialSuggestionsPromtp();
     const completion = await fetchCompletion(prompt);
@@ -34,21 +63,46 @@ function SelectMainTopic() {
     return 0;
   };
 
+  useEffect(() => {
+    if (isTopicsGenerated === false) {
+      setMainTopic("");
+    }
+    setIsBadKeyWordsGenerated(false);
+  }, []);
+
+  const handleNext = () => {
+    setActiveStep((cur) => cur + 1);
+    setIsBadKeyWordsGenerated(false);
+    setIsIntentionsGenerated(false);
+    setInitialBadKeywords([]);
+    setInitialIntentions([]);
+    setSelectedIntentions([]);
+    setSelectedBadKeywords([]);
+  }
   return (
     <div className="step-container">
       <h1 className="font-bold text-4xl">
         ¿Qué tipo de negocio estás desarrollando?
       </h1>
-      <div className="pt-10">
+      <div className="pt-10 flex">
         <input
           value={mainTopic}
-          onChange={(e) => handleFieldChange("lastname", e.target.value)}
+          onChange={(e) => handleFieldChange("keyword", e.target.value)}
           type="email"
           id="helper-text"
           aria-describedby="helper-text-explanation"
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
           placeholder="Escribe el tema principal de su sitio"
         />
+        <button
+          onClick={handleNext}
+          className={`mt-1 ml-2 btn-suggestions-topic focus:outline-none text-white font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900 animate-slide-top ${
+            mainTopic !== "" ? "" : "opacity-60"
+          }`}
+          disabled={mainTopic === ""}
+        >
+          Siguiente
+        </button>
       </div>
       <div className="block">
         <div className="flex">
