56c3ca893f0b783549a92f59225547ba1ba2001b
AllCz29 <allancor29@gmail.com>
2023-06-16 08:11:06 -0600

    create: component to share phrase

---
+++ seo-generator/src/root/components/SeoApp/ShareComponent.tsx
@@ -0,0 +1,85 @@
+import React from "react";
+import {
+  FaFacebookF,
+  FaFacebookMessenger,
+  FaInstagram,
+  FaWhatsapp,
+} from "react-icons/fa";
+import {} from "react-share";
+
+const ShareButtonsComponent = () => {
+  const shareUrl = "https://example.com"; // URL a compartir
+  const textToShare = "¡Mira este contenido increíble!"; // Texto a compartir
+
+  const handleShareClick = (event: any, network: any) => {
+    event.preventDefault();
+
+    const shareText = `${textToShare} ${shareUrl}`;
+
+    switch (network) {
+      case "facebook":
+        window.open(
+          `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(
+            shareUrl
+          )}`,
+          "_blank"
+        );
+        break;
+      case "instagram":
+        window.open(
+          `https://www.instagram.com/share?url=${encodeURIComponent(
+            shareUrl
+          )}&title=${encodeURIComponent(shareText)}`,
+          "_blank"
+        );
+        break;
+      case "whatsapp":
+        window.open(
+          `https://api.whatsapp.com/send?text=${encodeURIComponent(shareText)}`,
+          "_blank"
+        );
+        break;
+      default:
+        break;
+    }
+  };
+
+  return (
+    <div className="justify-center flex items-center">
+      <button
+        type="button"
+        data-te-ripple-init
+        data-te-ripple-color="light"
+        className="hover:scale-110 animate-slide-top mb-2 inline-block rounded-lg px-4 py-4 mr-3 text-xs font-medium uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg"
+        style={{ backgroundColor: "#3b5998" }}
+        onClick={(event) => handleShareClick(event, "facebook")}
+      >
+        <FaFacebookF />
+      </button>
+
+      <button
+        type="button"
+        data-te-ripple-init
+        data-te-ripple-color="light"
+        className="hover:scale-110 animate-slide-top mb-2 inline-block rounded-lg px-4 py-4 mr-3 text-xs font-medium uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg"
+        style={{ backgroundColor: "#d340b3" }}
+        onClick={(event) => handleShareClick(event, "instagram")}
+      >
+        <FaInstagram />
+      </button>
+
+      <button
+        type="button"
+        data-te-ripple-init
+        data-te-ripple-color="light"
+        className="hover:scale-110 animate-slide-top mb-2 inline-block rounded-lg px-4 py-4 text-xs font-medium uppercase leading-normal text-white shadow-md transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg"
+        style={{ backgroundColor: "#3ce85e" }}
+        onClick={(event) => handleShareClick(event, "whatsapp")}
+      >
+        <FaWhatsapp />
+      </button>
+    </div>
+  );
+};
+
+export default ShareButtonsComponent;
