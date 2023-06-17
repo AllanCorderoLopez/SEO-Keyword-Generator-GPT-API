702c92596df7e133d9cf325c0dd5bdc3f01acc42
AllCz29 <allancor29@gmail.com>
2023-06-17 12:35:20 -0600

    Update: navBar

--- seo-generator/src/root/components/NavBar/Navbar.tsx
+++ seo-generator/src/root/components/NavBar/Navbar.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import Image from "next/image";
-import logo from "../../../logo3.png";
+import logo from "../../../assets/img/logo3.png";
 
 function Navbar() {
   return (
-    <nav className="bg-white w-full bg-red-800 bg-color-nav border-gray-200 dark:bg-gray-900">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-
+    <nav className="bg-white shadow-lg z-10	  w-full bg-red-800 bg-color-nav border-gray-200 dark:bg-gray-900">
+      <div className="max-w-screen-xl   flex flex-wrap items-center justify-between mx-auto p-4">
+        <a href="#" className="flex items-center">
+          <Image
+            src={logo}
+            width={130}
+            height={130}
+            alt="logo"
+          />
+          </a>
         <button
           data-collapse-toggle="navbar-default"
           type="button"
@@ -31,18 +38,10 @@ function Navbar() {
         </button>
         <div className="hidden w-full md:block md:w-auto" id="navbar-default">
           <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0  dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+
             <li>
               <a
-                href="#"
-                className="block py-2 pl-3 pr-4 text-white bg-white rounded md:bg-transparent md:p-0 text-white text-white"
-                aria-current="page"
-              >
-                Inicio
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
+                href="#functions"
                 className="block py-2 pl-3 pr-4 text-white bg-white rounded md:bg-transparent md:p-0 text-white text-white"
                 aria-current="page"
               >
@@ -51,11 +50,20 @@ function Navbar() {
             </li>
             <li>
               <a
-                href="#"
+                href="#app"
                 className="block py-2 pl-3 pr-4 text-white bg-white rounded md:bg-transparent md:p-0 text-white text-white"
                 aria-current="page"
               >
                 App
+              </a>
+            </li>
+            <li>
+              <a
+                href="#IA"
+                className="block py-2 pl-3 pr-4 text-white bg-white rounded md:bg-transparent md:p-0 text-white text-white"
+                aria-current="page"
+              >
+                IA
               </a>
             </li>
         
